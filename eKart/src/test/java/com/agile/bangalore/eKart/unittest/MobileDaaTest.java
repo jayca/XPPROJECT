7051884395bbb208f1360fe7cba0d8232029dda9
ramanraghur <ramanraghu.r@gmail.com>
2015-08-05 23:52:34 +0530

    added basic unit test

---
+++ eKart/src/test/java/com/agile/bangalore/eKart/unittest/MobileDaaTest.java
@@ -0,0 +1,11 @@
+package com.agile.bangalore.eKart.unittest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MobileDaaTest {
+	@Test
+	public void addMobilesTest() {
+		Assert.assertTrue(true);
+	}
+}
